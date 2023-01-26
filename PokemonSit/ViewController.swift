64bbedd206451901ad80e398e567856415f890f1
RmanShch <man46ro@gmail.com>
2023-01-26 16:17:29 +0300

    initial commit

---
+++ PokemonSit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonSit
+//
+//  Created by Анна Шербицкая on 26.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
